bd5a3131a20362c92ebe6fa30d4cad330c7c1879
Nicole <not_4our@yahoo.com>
2013-03-15 10:47:08 +0800

    working basic jvectormap

---
+++ config/initializers/jvectormap.rb
@@ -0,0 +1,3 @@
+JVectorMap::Rails.precompile_maps << "us_merc_en"
+JVectorMap::Rails.precompile_maps << "ph_regions_mill_en"
+JVectorMap::Rails.precompile_maps << "world_mill_en"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vectormap::Application.config.secret_token = '04b33ac8a0c842c4be669c33b4fa3e2086d11c36098f030f5f5aabb3a450ef9c12128e4625845eb950ab811fb3fd7f9af0d0d35f349d4df14add51d2d78fe240'
